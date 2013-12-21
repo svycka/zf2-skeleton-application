85592f9cbf0bd3c608607c33e8e576720bf9ec97
grizzm0 <karlsson.kristofer@gmail.com>
2013-12-21 14:46:17 +0100

    Bootstrap glyphicons now working

--- module/Application/config/assetmanager.config.php
+++ module/Application/config/assetmanager.config.php
@@ -10,6 +10,12 @@ return [
             'asset/css/application.css' => __DIR__ .'/../asset/less/application.less',
             'asset/css/bootstrap.css'   => __DIR__ .'/../asset/less/bootstrap.less',
 
+            // Fonts
+            'asset/font/glyphicons-halflings-regular.eot'   => getcwd() .'/vendor/twitter/bootstrap/fonts/glyphicons-halflings-regular.eot',
+            'asset/font/glyphicons-halflings-regular.svg'   => getcwd() .'/vendor/twitter/bootstrap/fonts/glyphicons-halflings-regular.svg',
+            'asset/font/glyphicons-halflings-regular.ttf'   => getcwd() .'/vendor/twitter/bootstrap/fonts/glyphicons-halflings-regular.ttf',
+            'asset/font/glyphicons-halflings-regular.woff'  => getcwd() .'/vendor/twitter/bootstrap/fonts/glyphicons-halflings-regular.woff',
+
             // JS
             'asset/js/bootstrap.js'     => getcwd() .'/vendor/twitter/bootstrap/dist/js/bootstrap.js',
             'asset/js/html5shiv.js'     => getcwd() .'/vendor/html5shiv/html5shiv/html5shiv.js',
