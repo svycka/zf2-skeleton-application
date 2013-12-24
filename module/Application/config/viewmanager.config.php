7c177f8106f61e0ea1cbceeff678b822f79575ce
grizzm0 <karlsson.kristofer@gmail.com>
2013-12-24 11:00:32 +0100

    Reworked error messages

--- module/Application/config/viewmanager.config.php
+++ module/Application/config/viewmanager.config.php
@@ -2,14 +2,21 @@
 return [
     'doctype'               => 'HTML5',
     'not_found_template'    => 'error/404',
-    'exception_template'    => 'error/index',
+    'exception_template'    => 'error/500',
     'layout'                => 'layout/default',
     'template_map' => [
-        'layout/default'          => __DIR__ .'/../view/layout/default.phtml',
-        'error/404'               => __DIR__ .'/../view/error/404.phtml',
-        'error/index'             => __DIR__ .'/../view/error/index.phtml',
-
         // Application templates
-        'application/home/index'  => __DIR__ .'/../view/application/home/index.phtml',
+        'application/home/index'    => __DIR__ .'/../view/application/home/index.phtml',
+
+        // Error
+        'error/404'                 => __DIR__ .'/../view/error/404.phtml',
+        'error/500'                 => __DIR__ .'/../view/error/500.phtml',
+        'error/partial/controller'  => __DIR__ .'/../view/error/partial/controller.phtml',
+        'error/partial/exception'   => __DIR__ .'/../view/error/partial/exception.phtml',
+        'error/partial/not-found'   => __DIR__ .'/../view/error/partial/not-found.phtml',
+
+        // Layout
+        'layout/default'            => __DIR__ .'/../view/layout/default.phtml',
+        'layout/error'              => __DIR__ .'/../view/layout/error.phtml',
     ],
 ];
