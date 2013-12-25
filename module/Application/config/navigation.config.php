10ef61059493c74857289da01dccd9ed593b0ec5
grizzm0 <karlsson.kristofer@gmail.com>
2013-12-25 10:07:21 +0100

    Name attribute is obsolete in HTML5

--- module/Application/config/navigation.config.php
+++ module/Application/config/navigation.config.php
@@ -3,9 +3,6 @@ return [
     'containers' => [
         'navigation' => [
             [
-                'attributes' => [
-                    'name' => 'home',
-                ],
                 'options' => [
                     'label' => 'Home',
                     'route' => 'home',
