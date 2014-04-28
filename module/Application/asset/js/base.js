a37f37be5665290b0aa4ca59295db15126721f8a
Vytautas Stankus <svycka@gmail.com>
2014-04-28 09:56:23 +0300

    Fixed assetmanager config and added base.js for js code

---
+++ module/Application/asset/js/base.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+    // todo: JS logic here...
+});
--- module/Application/config/assetmanager.config.php
+++ module/Application/config/assetmanager.config.php
@@ -5,10 +5,12 @@ return [
             'asset/css/application.css' => [
                 'asset/css/bootstrap.css',
                 'asset/css/base.css',
+                'asset/css/theme.css',
             ],
 
-            'asset/js/prx.js' => [
+            'asset/js/application.js' => [
                 'asset/js/bootstrap.js',
+                'asset/js/base.js',
             ],
         ],
 
@@ -16,6 +18,11 @@ return [
             // CSS
             'asset/css/base.css'        => __DIR__ .'/../asset/less/base.less',
             'asset/css/bootstrap.css'   => __DIR__ .'/../asset/less/bootstrap.less',
+            'asset/css/theme.css'        => __DIR__ .'/../../../vendor/twitter/bootstrap/less/theme.less',
+
+            // JS
+            'asset/js/bootstrap.js'     =>__DIR__ . '/../../../vendor/twitter/bootstrap/dist/js/bootstrap.js',
+            'asset/js/base.js'          => __DIR__ .'/../asset/js/base.js',
         ],
     ],
 ];
